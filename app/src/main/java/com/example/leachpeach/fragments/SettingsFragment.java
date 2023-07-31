29c7e93af696f52ddd81783d14645278a8f7649e
Frankie915 <frankmm083@gmail.com>
2023-07-31 19:32:10 -0400

    theme added to SettingsFragment

--- app/src/main/java/com/example/leachpeach/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/leachpeach/fragments/SettingsFragment.java
@@ -1,17 +1,28 @@
 // SettingsFragment.java
 package com.example.leachpeach.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.example.leachpeach.R;
 
 public class SettingsFragment extends Fragment {
+
+    private static final String PREFS_NAME = "PrefsFile";
+    private Switch mThemeButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,5 +33,20 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Here, you can handle any initialization for this fragment
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        mThemeButton = view.findViewById(R.id.switch_theme);
+
+        mThemeButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Your code here based on the switch state (isChecked)
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
     }
 }

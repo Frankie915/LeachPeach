58cc33d8b4af6bd9d899ed760a8552bffd18abfc
Jonathan Diaz <jrd17j@fsu.edu>
2023-08-01 21:13:38 -0400

    Changed all variables to their new names

--- app/src/main/java/com/example/leachpeach/adapters/ExerciseViewAdapter.java
+++ app/src/main/java/com/example/leachpeach/adapters/ExerciseViewAdapter.java
@@ -14,19 +14,19 @@ import com.example.leachpeach.model.Exercise;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExerciseDetailAdapter extends RecyclerView.Adapter<ExerciseDetailAdapter.ExerciseDetailHolder> {
+public class ExerciseViewAdapter extends RecyclerView.Adapter<ExerciseViewAdapter.ExerciseViewHolder> {
 
     private List<Exercise> exercises = new ArrayList<>();
 
     @NonNull
     @Override
-    public ExerciseDetailHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ExerciseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.exercise_detail_item, parent, false);
-        return new ExerciseDetailHolder(itemView);
+        return new ExerciseViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ExerciseDetailHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ExerciseViewHolder holder, int position) {
         Exercise currentExercise = exercises.get(position);
         holder.editTextName.setText(currentExercise.getName());
         holder.editTextWeight.setText(String.valueOf(currentExercise.getWeight()));
@@ -44,13 +44,13 @@ public class ExerciseDetailAdapter extends RecyclerView.Adapter<ExerciseDetailAd
         notifyDataSetChanged();
     }
 
-    public class ExerciseDetailHolder extends RecyclerView.ViewHolder {
+    public class ExerciseViewHolder extends RecyclerView.ViewHolder {
         public EditText editTextName;
         public EditText editTextWeight;
         public EditText editTextSets;
         public EditText editTextReps;
 
-        public ExerciseDetailHolder(View itemView) {
+        public ExerciseViewHolder(View itemView) {
             super(itemView);
             editTextName = itemView.findViewById(R.id.exercise_name);
             editTextWeight = itemView.findViewById(R.id.exercise_weight);
